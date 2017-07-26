497c9b1789d7842012be0cdd24ca86df0ff5c146
Howard Kim <howkim2@yahoo.com>
2017-07-25 18:25:24 -0700

    player_cards

---
+++ data/dummy_data.js
@@ -0,0 +1,108 @@
+export const dummy_data = {
+  "playercards" : [
+    {
+      "name" : "Armada",
+      "main" : "Peach",
+      "id" : 1,
+      "image_url": "http://www.meleeitonme.com/wp-content/uploads/Armada2-300x2571.png"
+    },
+    {
+      "name" : "Amsa",
+      "main" : "Yoshi",
+      "id" : 2,
+      "image_url": "https://www.ssbwiki.com/images/thumb/c/c0/VGBC_aMSa.png/160px-VGBC_aMSa.png"
+    },
+    {
+      "tag" : "Android",
+      "main" : "Shiek",
+      "id" : 3,
+      "image_url": "http://wiki.teamliquid.net/commons/images/thumb/7/72/Androidpeanuts.jpg/600px-Androidpeanuts.jpg"
+    },
+    {
+      "tag": "Axe",
+      "main": "Pikachu",
+      "id" : 4,
+      "image_url": "http://wiki.teamliquid.net/commons/images/thumb/8/8c/Axe_apex_2015_closeup.jpg/600px-Axe_apex_2015_closeup.jpg"
+    },
+    {
+      "tag": "Mang0",
+      "main": "Fox",
+      "id" : 5,
+      "image_url": "https://media.eventhubs.com/images/tpd/player-pictures/thumb/mango.jpg"
+    },
+    {
+      "tag": "Leffen",
+      "main": "Fox",
+      "id" : 6,
+      "image_url": "https://image.redbull.com/rbcom/010/2016-05-02/1331792427656_2/0010/1/1500/1000/1/leffen-s-canadian-journey-has-begun.png"
+    },
+    {
+      "tag": "HungryBox",
+      "main": "Jigglypuff",
+      "id" : 7,
+      "image_url": "http://wiki.teamliquid.net/commons/images/thumb/9/92/Hboxmeru.jpg/600px-Hboxmeru.jpg"
+    },
+    {
+      "tag": "The Moon",
+      "main": "Marth",
+      "id" : 8,
+      "image_url": "https://slingshotesports.com/wp-content/uploads/2016/05/26876887965_42b5249a65_k.jpg"
+    },
+    {
+      "tag": "Mew2King",
+      "main": "Marth",
+      "id" : 9,
+      "image_url": "https://i.ytimg.com/vi/u9a2tWRF_mM/hqdefault.jpg"
+    },
+    {
+      "tag": "Westballz",
+      "main": "Falco",
+      "id": 10,
+      "image_url": "https://i.ytimg.com/vi/8VXxx-BwZik/maxresdefault.jpg"
+    }
+  ],
+
+  "playerprofiles" : [
+    {
+      "id": 1,
+      "tag": "Armada",
+      "image_url" : "http://www.meleeitonme.com/wp-content/uploads/Armada2-300x2571.png",
+      "rank" : 1,
+      "location": "Sweden",
+      "main": "Peach",
+      "secondary": "Fox",
+      "matches_played": 300,
+      "wins_vs_all": 290,
+      "wins_vs_top100": 150,
+      "wins_vs_top25": 70,
+      "wins_vs_top6": 20,
+      "nemesis": "Hungrybox",
+      "sponsor": "Alliance",
+      "twitter": "ArmadaUGS",
+      "twitch": "ArmadaUGS",
+      "most_recent_tournament": "EVO 2017",
+      "tidbit": "Armada is really good"
+    },
+    {
+      "id": 7,
+      "tag": "Hungrybox",
+      "image_url" : "http://wiki.teamliquid.net/commons/images/thumb/9/92/Hboxmeru.jpg/600px-Hboxmeru.jpg",
+      "rank" : 2,
+      "location": "Florida",
+      "main": "Jigglypuff",
+      "secondary": null,
+      "matches_played": 260,
+      "wins_vs_all": 255,
+      "wins_vs_top100": 100,
+      "wins_vs_top25": 30,
+      "wins_vs_top6": 20,
+      "nemesis": "Mang0",
+      "sponsor": "Liquid",
+      "twitter": "LiquidHbox",
+      "twitch": "hungrybox",
+      "most_recent_tournament": "Dreamhack Atlanta",
+      "tidbit": "Hungry has a degree in chemical engineering"
+    }
+  ]
+
+}
