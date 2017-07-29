cdcbf64808aa2c890473caf7b37a0dd57c766f08
David Sung <dks99455@gmail.com>
2017-07-28 17:45:50 -0700

    images importing component completed

---
+++ client/src/components/images.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+
+const images = importAll(require.context('./imgs', false, /\.(gif|png|jpe?g|svg)$/));
+
+// import images from './images';
+// <img src={images['landingpage.png']} />
+// <img src={images['logo.svg']} />
+// <img src={images['ProfilePlaceholder.gif']} />
+// <img src={images['SmashBrosSymbol.svg']} />
+
+export default images;
