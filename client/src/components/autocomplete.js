0951dd167a425451b8c7d0e6c388fab453c1432f
David Sung <dks99455@gmail.com>
2017-07-31 10:23:50 -0700

    link functionality added to autocomplete

--- client/src/components/autocomplete.js
+++ client/src/components/autocomplete.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import images from './images';
 import './stylish.css';
 
@@ -7,12 +8,13 @@ const Autocomplete = (props) => {
     const autocomItem = props.recommendations.map ((item, index) => {
 
         return (
-
-        <div className="autocomItem col-12" key={index}>
-            <div className="col-4">{item.name}</div>
-            <div className="col-4">{item.main}</div>
-            <div className="col-4"> <img className="autocomPic" src={item.image_url !== '' ? item.image_url : images['ProfilePlaceholder.gif']}/> </div>
-        </div>
+            <Link to={`/player_profile/${item.id}`} key={index}>
+                <div className="autocomItem col-12" key={index}>
+                    <div className="col-4">{item.name}</div>
+                    <div className="col-4">{item.main !== '' || null ? item.main : 'No Main'}</div>
+                    <div className="col-4"> <img className="autocomPic" src={item.image_url !== '' || null ? item.image_url : images['ProfilePlaceholder.gif']}/> </div>
+                </div>
+            </Link>
         )
     });
 
