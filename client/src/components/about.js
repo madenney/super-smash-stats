6be0c20acf76c4f9a98a75e667cf75f1dd3c768a
Adam Luong <adamaluong@gmail.com>
2017-08-03 10:48:30 -0700

    fixed some styling

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -7,7 +7,10 @@ const About = () => {
     return (
         <div className="container">
             <h1> About Us </h1>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <div className="description">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </div>
+            <p></p>
 
             <div className="card-row">
                 <div className="card">
@@ -16,19 +19,19 @@ const About = () => {
                 </div>
                 <div className="card">
                     <img className="card-img-top" src="http://via.placeholder.com/300.png/09f/fff"/>
-                    <span className="name">Person 1</span>
+                    <span className="name">Person 2</span>
                 </div>
                 <div className="card">
                     <img className="card-img-top" src="http://via.placeholder.com/300.png/09f/fff"/>
-                    <span className="name">Person 1</span>
+                    <span className="name">Person 3</span>
                 </div>
                 <div className="card">
                     <img className="card-img-top" src="http://via.placeholder.com/300.png/09f/fff"/>
-                    <span className="name">Person 1</span>
+                    <span className="name">Person 4</span>
                 </div>
                 <div className="card">
                     <img className="card-img-top" src="http://via.placeholder.com/300.png/09f/fff"/>
-                    <span className="name">Person 1</span>
+                    <span className="name">Person 5</span>
                 </div>
             </div>
 
