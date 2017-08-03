e6f71812f1fda27d7c3808809bde11a8b6914c56
Adam Luong <adamaluong@gmail.com>
2017-08-02 19:01:09 -0700

    about us page

---
+++ client/src/components/about.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+        <div class="container">
+            <div class="about">
+                <h1>About Us</h1>
+                <div class="description">
+                    <p>Lorem ipsum dolor sit amet, omnes scaevola scripserit ius cu, odio aliquip eloquentiam nam at. Meliore hendrerit no nec. Partiendo interesset sit ut, clita volumus ei duo. Eu duo graeco ceteros disputationi, et mea erat malorum mentitum. Eos an noster eloquentiam, facete nostrum at ius.</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="http://via.placeholder.com/300.png/09f/fff"/>
+                <span>Person 1</span>
+                <span>Developer</span>
+            </div>
+            <div class="card">
+                <img src="http://via.placeholder.com/300.png/09f/fff"/>
+                <span>Person 2</span>
+                <span>Developer</span>
+            </div>
+            <div class="card">
+                <img src="http://via.placeholder.com/300.png/09f/fff"/>
+                <span>Person 3</span>
+                <span>Developer</span>
+            </div>
+            <div class="card">
+                <img src="http://via.placeholder.com/300.png/09f/fff"/>
+                <span>Person 4</span>
+                <span>Developer</span>
+            </div>
+            <div class="card">
+                <img src="http://via.placeholder.com/300.png/09f/fff"/>
+                <span>Person 5</span>
+                <span>Developer</span>
+            </div>
+        </div>
+    )
+};
+
+export default About;
