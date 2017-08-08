9e9b3c339ea3c68fdddc3317f778d5bd6612f2dc
Adam Luong <adamaluong@gmail.com>
2017-08-07 17:17:55 -0700

    about us- adam luong

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -6,39 +6,40 @@ import David from './imgs/developer_pics/david.jpg';
 import Adam from './imgs/developer_pics/adam.jpg';
 
 
-
 const About = () => {
-
     return (
         <div className="container">
             <h1> About Us </h1>
             <div className="description">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum."
             </div>
-
             <div className="card-row">
                 <div className="card">
                     <img className="card-img-top" src={Matt}/>
-                    <span className="name">Matt "Mad</span>
+                    <div className="after">Matt "Mad Matt" Denney</div>
                 </div>
                 <div className="card">
                     <img className="card-img-top" src={Howard}/>
-                    <span className="name">Person 2</span>
-                </div>
-                <div className="card">
-                    <img className="card-img-top" src={David}/>
-                    <span className="name">Person 3</span>
-                </div>
-                <div className="card">
-                    <img className="card-img-top" src={Adam}/>
-                    <span className="name">Person 4</span>
+                    <div className="after">Howard "Hun" Kim</div>
                 </div>
                 <div className="card">
                     <img className="card-img-top" src="http://via.placeholder.com/300.png/09f/fff"/>
-                    <span className="name">Person 5</span>
+                    <span className="after">Khanh Nguyen</span>
+                </div>
+                <br/>
+                <div className="card">
+                    <img className="card-img-top" src={David}/>
+                    <div className="after">David Sung</div>
+                </div>
+                <div className="card">
+                    <img className="card-img-top" src={Adam}/>
+                    <div className="after">Adam Luong</div>
                 </div>
             </div>
-
         </div>
     )
 };
