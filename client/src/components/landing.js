25c2c2b37e59b87c5786a8a5a488b786949ecded
David Sung <dks99455@gmail.com>
2017-08-09 12:02:37 -0700

    navbar search fixed

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import Searchbar from './searchbar';
+import LandLogo from './imgs/land_logo.png';
 import axios from 'axios';
-import Navbar from './navbar';
-import PlayerProfile from './player_profile';
-import SearchResults from './search_results';
-import {Route, Link} from 'react-router-dom';
+import Carousel from './playercardcarousel';
 import './stylish.css';
 
 export default class Landingpage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: '',
+            autocomCards: [],
+            cards: ''
+        };
+    }
+    componentWillMount(){
+        axios.post('http://localhost:3030/front_page', {number: 6}).then((response)=>{
+            // console.log('this is response: ', response);
+            this.setState({
+                cards: response.data
+            })
+        });
+    }
+
     render() {
         return (
             <div className="landing">
-              <Searchbar />
+                <div className ='container center'>
+                    <div className='landpage_logo row offset-md-3 col-md-6 off-lg-4'>
+                        <img src = {LandLogo}/>
+                    </div>
+                    <Searchbar />
+                    {/* Row for the player cards styling */}
+                    <div className='row col-md-6 offset-md-3 col-sm-9 offset-sm-1 scrollmenu'>
+                        <Carousel card = {this.state.cards} />
+                    </div>
+                </div>
             </div>
         )
     }
