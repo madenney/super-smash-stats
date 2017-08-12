086bdb9a07014647a1f889600adb9744cd1db282
Howard Kim <howkim2@yahoo.com>
2017-08-12 16:33:44 -0700

    styled the front page landpage

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -5,29 +5,52 @@ import SmashIcon from './imgs/SmashBrosSymbol.svg';
 import NavBarSearch from './navbar_search';
 
 const Navbar = () => {
-
+    const ulStyle = {
+      width: '80%'
+    };
     return(
       <nav className='navbar navbar-inverse bg-inverse navbar-static-top navbar-toggleable'>
           <Link to="/" className="navbar-brand homeIcon">
             <img src={SmashIcon} />
           </Link>
-          <ul className='navbar-nav'>
+          <ul className='nav nav-justified mx-auto' style={ulStyle} >
+            <li className='nav-item nav-link'>
+              <Link to='/'>
+                  <div className='navButton'>
+                      Home
+                  </div>
+              </Link>
+            </li>
             <li className='nav-item nav-link'>
               <Link to='#'>
                   <div className='navButton'>
-                      H2H
+                      Featured
+                  </div>
+              </Link>
+            </li>
+            <li className='nav-item nav-link'>
+              <Link to='/results/noSearch/1'>
+                  <div className='navButton'>
+                      Top Players
+                  </div>
+              </Link>
+            </li>
+            <li className='nav-item nav-link'>
+              <Link to='/faq'>
+                  <div className='navButton'>
+                      FAQ
                   </div>
               </Link>
             </li>
             <li className='nav-item nav-link'>
               <Link to='/about'>
                   <div className='navButton'>
-                      About Us
+                      About
                   </div>
               </Link>
             </li>
           </ul>
-          <NavBarSearch />
+          {/* <NavBarSearch /> */}
       </nav>
     )
 
