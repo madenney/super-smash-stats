e689700795082b2809e83d5adb3ee2a10130b56e
Matt <matt.a.denney@gmail.com>
2017-08-14 20:12:08 -0700

    cleaned up some playercard code - Matt

--- client/src/components/populateplayercards.js
+++ client/src/components/populateplayercards.js
@@ -3,47 +3,37 @@ import {Link} from 'react-router-dom';
 import images from './images';
 
 const PopulatePlayerCards = (props) => {
-  // console.log('cards props:', props);
-  if(!props.card || Object.keys(props.card).length < 1){
-    return(
-      <h1>Loading...</h1>
-    );
-  }
-  const player_cards = props.card.map((item, index)=>{
-    if(!images[`player_pic/${item.tag}.png`]){
-      let imageUrl = images['ProfilePlaceholder.gif'];
-      let divStyle = {
-        backgroundImage: 'url(' + imageUrl +')',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center'
-      }
-      return(
-          <Link to={`/player_profile/${item.id}`}  key={index}>
-              <div className='col-md-2 player_card' style={divStyle}>
-                    <h3 className='player_text'>{item.tag}</h3>
-              </div>
-          </Link>
-      )
+    // console.log('cards props:', props);
+    if(!props.card || Object.keys(props.card).length < 1){
+        return(
+            <h1>Loading...</h1>
+        );
     }
-    else{
-      let imageUrl = images[`player_pic/${item.tag}.png`];
-      let divStyle = {
-        backgroundImage: 'url(' + imageUrl +')',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center'
-      }
-      return(
-          <Link to={`/player_profile/${item.id}`} key={index}>
-              <div className='col-md-2 player_card' style={divStyle}>
-                    <h3 className='player_text'>{item.tag}</h3>
-              </div>
-          </Link>
-      )
-    }
-  });
 
-  return (
+    const player_cards = props.card.map((item, index)=>{
+
+        let imageUrl = images['ProfilePlaceholder.gif'];
+        if(images[`player_pic/${item.tag}.png`]) {
+            imageUrl = images[`player_pic/${item.tag}.png`];
+        }
+
+        let divStyle = {
+            backgroundImage: 'url(' + imageUrl + ')',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center top'
+        };
+
+        return(
+            <Link to={`/player_profile/${item.id}`}  key={index}>
+                <div className='col-md-2 player_card' style={divStyle}>
+                    <h3 className='player_text'>{item.tag}</h3>
+                </div>
+            </Link>
+        )
+    });
+
+    return (
         <div className='container offset-md-1'>{player_cards}</div>
-  )
+    )
 };
 export default PopulatePlayerCards;
