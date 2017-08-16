82c1580a4a3975206ae3b477153d2cb175666226
David Sung <dks99455@gmail.com>
2017-08-15 17:58:40 -0700

    more styling

---
+++ client/src/components/faq.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Faq = () => {
+    return(
+        <div>
+            <div>What should I ask?</div>
+            <div>ionno. come back when you figure it out</div>
+        </div>
+    )
+};
+
+export default Faq;
