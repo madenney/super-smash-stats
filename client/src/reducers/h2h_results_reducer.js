d010ae220ae53681f735fde3eb2922a4a0e2d829
Dennis Dang <dangggdennis@gmail.com>
2017-09-06 16:37:05 -0700

    completed h2h profile component redux

--- client/src/reducers/h2h_results_reducer.js
+++ client/src/reducers/h2h_results_reducer.js
@@ -1,12 +1,14 @@
-import types from '../actions/types';
-const DEFAULT_STATE = {h2h_results: null}
+import types from "../actions/types";
+const DEFAULT_STATE = { h2h_results: null };
 
-export default function(state = DEFAULT_STATE, action){
-  switch(action.type){
-    case types.GET_H2H_RESULTS:
-      return {...state, h2h_results: action.payload};
-      break;
-    default:
-      return state;
-  }
+export default function(state = DEFAULT_STATE, action) {
+	switch (action.type) {
+		case types.GET_H2H_RESULTS:
+			return { ...state, h2h_results: action.payload };
+			break;
+		case types.GET_H2H_PROFILES:
+			return { ...state, profiles_results: action.payload };
+		default:
+			return state;
+	}
 }
