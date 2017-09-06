c95980409c0213058cabc5ac5ff589936aef50fc
Dennis Dang <dangggdennis@gmail.com>
2017-09-06 11:26:10 -0700

    refactoring css names

--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react';
-import Searchbar from './searchbar';
-import LandLogo from '../imgs/land_logo.png';
-import axios from 'axios';
-
-import {Link} from 'react-router-dom';
-import '../css/stylish.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Searchbar from "./searchbar";
+import landingPageLogo from "../imgs/land_logo.png";
+import "../css/landing_page.css";
 
 export default class Landingpage extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            value: '',
+            value: "",
             autocomCards: [],
-            cards: ''
+            cards: ""
         };
     }
     render() {
         return (
-            <div className="landing fromDarkness">
-                <div className ='container landingCenter'>
-                    <div className='landpage_logo row offset-sm-2 offset-md-4 col-md-9 off-lg-4 offset-xl-3 col-12'>
-                        <img src = {LandLogo}/>
+            <div className="landingPage--darkness">
+                <div className="container landingPage--center">
+                    <div className="landingPage-logo row offset-sm-2 offset-md-4 col-md-9 off-lg-4 offset-xl-3 col-12">
+                        <img src={landingPageLogo} />
                     </div>
                     <div className="row row-eq-height">
                         <Searchbar history={this.props.history} />
                     </div>
-                    <br/>
-                    <Link to='/results/noSearch/1'>
-                        <p className="blinkText offset-xl-6 offset-lg-6 offset-md-6 offset-sm-4 offset-2">SEARCH THE DATABASE</p>
+                    <br />
+                    <Link to="/results/noSearch/1">
+                        <p className="text--blink offset-xl-6 offset-lg-6 offset-md-6 offset-sm-4 offset-2">
+                            SEARCH THE DATABASE
+                        </p>
                     </Link>
                 </div>
-                <div className='container character-selection-screen' id='character-selection-screen'>
-                  {/* <h1>Feature Set</h1> */}
-                </div>
             </div>
-        )
+        );
     }
 }
