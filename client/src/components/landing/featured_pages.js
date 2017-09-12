04c48d940985d88a6b70d4ad68007e072dfd7b59
Howard Kim <howkim2@yahoo.com>
2017-09-11 17:40:37 -0700

    changed featured pages-howard

--- client/src/components/landing/featured_pages.js
+++ client/src/components/landing/featured_pages.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class FeaturedPages extends Component {
   render(){
     return(
-      <h1>Featured Fucking Pages!</h1>
+      <h1>Featured Pages!</h1>
     )
   }
 }
