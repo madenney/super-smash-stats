f2a21ee0e770a5e861c993ea22f424d45b74cefd
Howard Kim <howkim2@yahoo.com>
2017-09-17 18:23:50 -0700

    added animations to player profile

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -122,6 +122,7 @@ export function getH2HResults(id1, search) {
 }
 
 export function getH2HProfiles(id1, id2) {
+	// console.log('id 1 id2: ', id1, id2);
 	return dispatch => {
 		axios.post("/head2headprofile", { id1, id2 }).then(response => {
 			console.log("h2hprofile results", response);
