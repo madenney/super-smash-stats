9c0cf2fdb562e1d02bccd1ce81bd5d8ec53a637e
Dennis Dang <dangggdennis@gmail.com>
2017-09-18 18:20:57 -0700

    added 404

---
+++ client/src/components/static_pages/error_page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+	return (
+		<div className="container">
+			<h1 className="text-center">
+				This page does not exist. Please back to home.
+			</h1>
+			<h2 className="text-center">Thank you!</h2>
+		</div>
+	);
+};
