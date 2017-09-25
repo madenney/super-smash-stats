121272cfc37ea28a6b6ef0ae9f73c904a161d9b9
Howard Kim <howkim2@yahoo.com>
2017-09-25 15:14:32 -0700

    applied youtube redux and etc having problems w componentwillreceiveprops-Howard

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,5 +5,6 @@ export default {
 	FILTER_PLAYER_TOURNAMENT: "filter_player_tournament",
 	GET_H2H_RESULTS: "get_h2h_results",
 	GET_H2H_PROFILES: "get_h2h_profiles",
-	GET_STICKY_VIDEO: "get_sticky_video"
+	GET_STICKY_VIDEO: "get_sticky_video",
+	CHECK_STICKY_VIDEO: "check_sticky_video"
 };
